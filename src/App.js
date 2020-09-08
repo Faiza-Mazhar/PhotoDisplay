2729c4d60fe8f74c331f4e75713c730fbb6e1b76
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2020-09-08 09:39:30 +0100

    Moved images to the public folder as brower fetches with relative to public folder.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import { PhotoPanel } from './PhotoPanel';
 function App(props) {
   let photoPanels = props.pictures.map((picture, index) => {
     return <PhotoPanel {...picture} key = {index}/>
-})
-return (<React.Fragment> 
-  <h1>Photos</h1>
-  
-  {photoPanels}
-  
-  </React.Fragment>)
+  })
+  return (<React.Fragment> 
+    <h1>Photos</h1>
+      {photoPanels}
+    </React.Fragment>
+  )
 }
 
 export default App;
--- src/Image.js
+++ src/Image.js
@@ -2,9 +2,7 @@ import React from 'react';
 export const Image = ({ pic, label }) => {
   return (
     <div>
-  
-      <img src={require(`${pic}`)} alt={label} className='image' />
+      <img src={pic} alt={label} className='image' />
     </div>
   );
-
 };
